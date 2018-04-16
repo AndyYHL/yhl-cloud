47a37da40e2f3fd6f4f1b464f4aa3b842050b6ea
Andy <love90yhl@hotmail.com>
2018-04-16 15:01:21 +0800

    客户端负载均衡 Ribbon

--- ribbon/src/main/java/com/yhl/ribbon/RibbonApplication.java
+++ ribbon/src/main/java/com/yhl/ribbon/RibbonApplication.java
@@ -2,9 +2,20 @@ package com.yhl.ribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient //注册客户端
 public class RibbonApplication {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RibbonApplication.class, args);
---
+++ ribbon/src/main/java/com/yhl/ribbon/controller/RibbonContriller.java
@@ -0,0 +1,28 @@
+package com.yhl.ribbon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class RibbonContriller {
+    @Autowired
+    RestTemplate restTemplate; //注入负载
+
+    public String productHello(){
+        return restTemplate.getForEntity(" http://product-service/hello",String.class).getBody();
+    }
+
+    public String publisherHello(){
+        return restTemplate.getForEntity("http://compute-service/hello",String.class).getBody();
+    }
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public ResponseEntity<String> hello(){
+        return new ResponseEntity<>("Hello Ribbon-service", HttpStatus.OK);
+    }
+}
