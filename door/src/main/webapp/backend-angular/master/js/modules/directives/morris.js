0be307599628c347e6135bf67db69ab1a8df1458
Andy <love90yhl@hotmail.com>
2018-05-22 13:49:33 +0800

    加入网站模式

---
+++ door/src/main/webapp/backend-angular/master/js/modules/directives/morris.js
@@ -0,0 +1,43 @@
+/**=========================================================
+ * Module: morris.js
+ * AngularJS Directives for Morris Charts
+ =========================================================*/
+
+(function() {
+    "use strict";
+
+    AppH.directive('morrisBar',   morrisChart('Bar')   );
+    AppH.directive('morrisDonut', morrisChart('Donut') );
+    AppH.directive('morrisLine',  morrisChart('Line')  );
+    AppH.directive('morrisArea',  morrisChart('Area')  );
+
+    function morrisChart(type) {
+      return function () {
+        return {
+          restrict: 'EA',
+          scope: {
+            morrisData: '=',
+            morrisOptions: '='
+          },
+          link: function($scope, elem, attrs) {
+            // start ready to watch for changes in data
+            $scope.$watch("morrisData", function(newVal, oldVal) {
+              if (newVal) {
+                $scope.morrisInstance.setData(newVal);
+                $scope.morrisInstance.redraw();
+              }
+            }, true);
+            // the element that contains the chart
+            $scope.morrisOptions.element = elem;
+            // If data defined copy to options
+            if($scope.morrisData)
+              $scope.morrisOptions.data = $scope.morrisData;
+            // Init chart
+            $scope.morrisInstance = new Morris[type]($scope.morrisOptions);
+
+          }
+        }
+      }
+    }
+
+})();
